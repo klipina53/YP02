11c9d1cfcd650a1720ebe7ca840d057f0d2e2951
twozziey <twozziey@mail.ru>
2025-02-15 12:14:31 +0500

    Добавьте файлы проекта.

---
+++ YP.02/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YP._02
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+         
+        }
+
+
+
+        private void Entrance(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Forgot_my_password(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            placeholderText.Visibility = Visibility.Collapsed;
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(password.Password))
+            {
+                placeholderText.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(password.Password))
+            {
+                placeholderText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                placeholderText.Visibility = Visibility.Collapsed;
+            }
+        }
+    
+
+        private void UsernameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (usernameTextBox.Text == "Username")
+            {
+                usernameTextBox.Text = ""; 
+                usernameTextBox.Foreground = Brushes.White; 
+            }
+        }
+
+        private void UsernameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(usernameTextBox.Text))
+            {
+                usernameTextBox.Text = "Username"; 
+                usernameTextBox.Foreground = Brushes.LightGray; 
+            }
+        }
+    }
+}
